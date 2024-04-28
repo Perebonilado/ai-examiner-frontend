5e019563a398270f150fbb6a4ed29e4cdeaa9472
perebonilado <reradiri@eminenttechnology.com>
2024-04-28 22:37:36 +0100

    fix

--- api-services/questions.service.ts
+++ api-services/questions.service.ts
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { API_BASE_URL } from "../constants";
-import { secondsToMilliSeconds } from "../utils";
 import { QuestionsModel } from "@/models/questions.model";
 import { QuestionsDto } from "@/dto/questions.dto";
 

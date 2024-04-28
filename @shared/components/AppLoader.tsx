7f0c0dcd286a6c17edf6d30619702d52bb79d7de
perebonilado <reradiri@eminenttechnology.com>
2024-04-28 22:45:42 +0100

    css fix

--- @shared/components/AppLoader.tsx
+++ @shared/components/AppLoader.tsx
@@ -9,7 +9,7 @@ export const AppLoader: FC<Props> = ({ loaderMessage }) => {
     <div className="flex flex-col gap-4 justify-center items-center h-screen fixed z-[8000] top-0 left-0 w-screen bg-black bg-opacity-80">
       <div className="border-8 border-solid border-gray-300 border-t-[#324fa5] rounded-full w-12 h-12 animate-spin"></div>
       {loaderMessage && (
-        <p className="w-full max-w-[500px] mx-auto text-center font-semibold text-white">
+        <p className="w-full max-w-[500px] px-3 mx-auto text-center max-md:text-sm font-semibold text-white">
           {loaderMessage}
         </p>
       )}

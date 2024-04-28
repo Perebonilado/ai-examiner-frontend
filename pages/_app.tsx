18d4d60c2234d4bfb7e30826019d5863da701ae7
perebonilado <reradiri@eminenttechnology.com>
2024-04-28 08:47:45 +0100

    modules setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { reduxStore } from "@/config/redux-config";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={reduxStore}>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </Provider>
+  );
 }

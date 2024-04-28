71625c429f8caadf367b5f55be7a5a17ef362aa5
perebonilado <reradiri@eminenttechnology.com>
2024-04-28 21:58:49 +0100

    label fix

--- @shared/components/FileUpload.tsx
+++ @shared/components/FileUpload.tsx
@@ -40,7 +40,7 @@ const FileUpload: FC<Props> = ({
           name="file"
           types={allowedTypes}
           maxSize={maxFileSizeMB}
-          label="File Upload"
+          label=" Upload"
           onSizeError={() => {
             toast.error("Maximum File size allowed is 10mb");
           }}

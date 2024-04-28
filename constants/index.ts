149838abb8f6ab8a2eeee1a0dabca6ffeba1aa57
perebonilado <reradiri@eminenttechnology.com>
2024-04-28 20:20:53 +0100

    base url

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}`;
+export const API_BASE_URL = `https://ai-examiner-production.up.railway.app/api/v1`;
 
 export const milliSecondToSecondConversionRate = 1000;
 

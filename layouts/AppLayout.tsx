87ede703de7ba7f58a10b5edcd5f3c19b95742f3
perebonilado <reradiri@eminenttechnology.com>
2024-05-12 18:49:26 +0100

    bg fix

--- layouts/AppLayout.tsx
+++ layouts/AppLayout.tsx
@@ -4,7 +4,7 @@ import React, { FC, PropsWithChildren } from "react";
 
 const AppLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="flex h-screen overflow-hidden bg-slate-50">
+    <div className="flex h-screen overflow-hidden bg-slate-100">
       <Sidebar />
       <main className="h-screen w-[calc(100%-300px)] max-md:w-full overflow-auto pb-40 pt-[2rem] px-4">
         <FadeIn>{children}</FadeIn>

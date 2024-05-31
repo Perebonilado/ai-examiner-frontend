ad400ffe4aed109cb8acbcd5ae8507085a9962b3
perebonilado <reradiri@eminenttechnology.com>
2024-05-31 21:16:03 +0100

    test fix

--- layouts/AppLayout.tsx
+++ layouts/AppLayout.tsx
@@ -4,9 +4,9 @@ import React, { FC, PropsWithChildren } from "react";
 
 const AppLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="flex h-screen overflow-hidden bg-white border-blue-700 border-[4px]">
+    <div className="flex h-screen overflow-hidden bg-white border-blue-700 border-[4px] relative">
       <Sidebar />
-      <main className="h-screen w-[calc(100%-300px)] max-md:w-full overflow-auto pb-40 pt-[1.2rem] px-10 max-md:px-4">
+      <main className="h-full w-[calc(100%-300px)] max-md:w-full overflow-auto pb-40 pt-[1.2rem] px-10 max-md:px-4">
         <FadeIn>{children}</FadeIn>
       </main>
     </div>

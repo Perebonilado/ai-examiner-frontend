3cd46f317c1533f64ac5ea26a9208d41c466147a
perebonilado <reradiri@eminenttechnology.com>
2024-05-31 21:27:06 +0100

    removed borders

--- @shared/components/Sidebar/Sidebar.tsx
+++ @shared/components/Sidebar/Sidebar.tsx
@@ -53,7 +53,7 @@ const Sidebar: FC = () => {
         </div>
       }
       <aside className={sideBarContainerStyling}>
-        <div className="h-full relative p-2 border-[5px] border-rose-700">
+        <div className="h-full relative p-2">
           <div className="absolute bg-[#2F004F] h-[100%] w-[100%] px-4 max-md:top-0 top-0 left-1/2 -translate-x-1/2">
             <div className="h-[150px] flex pt-6 justify-center">
               <AppLogoAlt />
--- layouts/AppLayout.tsx
+++ layouts/AppLayout.tsx
@@ -4,7 +4,7 @@ import React, { FC, PropsWithChildren } from "react";
 
 const AppLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="flex h-screen overflow-hidden bg-white border-blue-700 border-[4px] relative">
+    <div className="flex h-screen overflow-hidden bg-white relative">
       <Sidebar />
       <main className="h-full w-[calc(100%-300px)] max-md:w-full overflow-auto pb-40 pt-[1.2rem] px-10 max-md:px-4">
         <FadeIn>{children}</FadeIn>

584605d72044e9843be22c58cddf0e93e188ab90
perebonilado <reradiri@eminenttechnology.com>
2024-06-22 22:51:07 +0100

    mobile fix

--- @modules/home/DescriptionItem.tsx
+++ @modules/home/DescriptionItem.tsx
@@ -28,7 +28,7 @@ const DescriptionItem: FC<Props> = ({ title, body, isEven = false, videoSrc }) =
           <div className="overflow-hidden max-h-[690px] flex items-center">
             <video
               autoPlay={true}
-              muted={true}
+              playsInline={true}
               loop={true}
               className="!bg-transparent"
             >

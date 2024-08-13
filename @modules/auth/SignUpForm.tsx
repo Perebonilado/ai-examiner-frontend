fd41eb7612b095f71e2e0cd8da11e6480ebb2a2b
perebonilado <perebonilado@gmail.com>
2024-08-13 21:52:52 +0100

    favicon change

--- @modules/auth/SignUpForm.tsx
+++ @modules/auth/SignUpForm.tsx
@@ -42,7 +42,7 @@ const SignUpForm: FC = () => {
       if (returnUrl) {
         router.push(decodeURIComponent(returnUrl as string));
       } else {
-        router.push("/new-document");
+        router.push("/pricing");
       }
     }
   }, [data]);

4ba82761c6345d33f4cfb1547acf9038cb606ce4
perebonilado <perebonilado@gmail.com>
2024-09-09 23:06:13 +0100

    ham fix

--- @shared/components/MobileAppNav.tsx
+++ @shared/components/MobileAppNav.tsx
@@ -12,7 +12,7 @@ const MobileAppNav: FC<Props> = ({ handleClick, isSideNav }) => {
   return (
     <nav className="bg-[#2F004F] px-4 h-[80px] w-full fixed z-50 flex items-center md:hidden">
       <div className="px-0 flex justify-between items-center w-full">
-        <div className="flex items-center gap-3">
+        <div className="flex items-center gap-5">
           <Hamburger isSideNavOpen={isSideNav} onClick={handleClick} />
           <AppLogoAlt size="sm"/>
         </div>

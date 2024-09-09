0d2eeff7db56d7190699ab0e5ff6b6ba776734b6
perebonilado <perebonilado@gmail.com>
2024-09-09 18:08:49 +0100

    middle ware augment fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,35 @@ import DescriptionItemContainer from "@/@modules/home/DescriptionItemContainer";
 import TestKnowledge from "@/@modules/home/TestKnowledge";
 import FAQContainer from "@/@modules/home/FAQContainer";
 import Footer from "@/@shared/components/Footer";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import { accessToken } from "@/constants";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const [userIsLoggedIn, setUserIsLoggedIn] = useState(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    handleRouteUserOnAuthenticated();
+  }, []);
+
+  useEffect(() => {
+    if (userIsLoggedIn) {
+      router.push("/new-document");
+    }
+  }, [userIsLoggedIn]);
+
+  const handleRouteUserOnAuthenticated = () => {
+    const token = Cookies.get(accessToken);
+
+    if (token) {
+      setUserIsLoggedIn(true);
+    } else {
+      setUserIsLoggedIn(false);
+    }
+  };
   return (
     <>
       <AppHead />

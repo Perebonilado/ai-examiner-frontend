d7ba257359ec4429c268099cf3611049910b8e0a
perebonilado <perebonilado@gmail.com>
2024-09-12 09:20:29 +0100

    moved to app level

--- utils/index.ts
+++ utils/index.ts
@@ -59,19 +59,7 @@ export const getFileNameWithoutExtension = (name: string) => {
 };
 
 export const convertPDFToTxt = async (file: File) => {
-  if (typeof Promise.withResolvers === "undefined") {
-    if (window)
-      // @ts-expect-error This does not exist outside of polyfill which this is doing
-      window.Promise.withResolvers = function () {
-        let resolve, reject;
-        const promise = new Promise((res, rej) => {
-          resolve = res;
-          reject = rej;
-        });
-        return { promise, resolve, reject };
-      };
-  }
-  
+
   const extension = file.name.split(".").pop();
   const fileName =
     file.name.substring(0, file.name.lastIndexOf(".")) || file.name;

53e28db1f5424412d1062df1cb8b17c519700a93
perebonilado <perebonilado@gmail.com>
2024-10-19 08:30:37 +0100

    home page animations

--- @modules/home/SupportLearningItem.tsx
+++ @modules/home/SupportLearningItem.tsx
@@ -1,3 +1,4 @@
+import TransitionUp from "@/transitions/TransitionUp";
 import React, { FC } from "react";
 
 export interface ISupportLearningItem {
@@ -12,13 +13,15 @@ const SupportLearningItem: FC<ISupportLearningItem> = ({
   title,
 }) => {
   return (
-    <div className="w-full max-w-[270px] h-[380px] border border-[#9D6EC2] rounded-xl shadow-xl p-6">
-      <div className="h-[40%]">{icon}</div>
-      <div className="h-[60%] flex flex-col gap-6">
-        <h3 className="text-xl font-bold">{title}</h3>
-        <p className="text-sm">{body}</p>
+    <TransitionUp>
+      <div className="w-full max-w-[270px] h-[380px] border border-[#9D6EC2] rounded-xl shadow-xl p-6">
+        <div className="h-[40%]">{icon}</div>
+        <div className="h-[60%] flex flex-col gap-6">
+          <h3 className="text-xl font-bold">{title}</h3>
+          <p className="text-sm">{body}</p>
+        </div>
       </div>
-    </div>
+    </TransitionUp>
   );
 };
 
---
+++ transitions/TransitionRight.tsx
@@ -0,0 +1,24 @@
+import React, { FC, PropsWithChildren } from "react";
+import { motion } from "framer-motion";
+
+interface Props {
+  className?: string;
+}
+
+const TransitionRight: FC<PropsWithChildren<Props>> = ({
+  children,
+  className = "",
+}) => {
+  return (
+    <motion.div
+      initial={{ translateX: -20 }}
+      whileInView={{ translateX: 0, transition: { duration: 0.8 } }}
+      viewport={{ once: true }}
+      className={className}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default TransitionRight;

2a9d62e23bd0fde48bfae8f7d61d67e9e3f8a60d
perebonilado <perebonilado@gmail.com>
2024-11-17 21:50:36 +0100

    timer

--- @modules/questions/CountDownTimer.tsx
+++ @modules/questions/CountDownTimer.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import cn from 'classnames'
 
 interface CountdownProps {
   timeLeft: number; // Time in seconds
@@ -27,13 +28,18 @@ const CountdownTimer: React.FC<CountdownProps> = ({
     setProgress(progressPercentage);
   }, [timeLeft, totalDuration]);
 
+  const strokeStyle = cn({
+    ['stroke-[#2F004F]']: progress > 20,
+    ['stroke-rose-600']: progress < 20
+  })
+
   // Calculate circle properties
   const radius = 35;
   const circumference = 2 * Math.PI * radius;
   const strokeDashoffset = circumference - (progress / 100) * circumference;
 
   return (
-    <div className="fixed top-20 z-[100] right-5">
+    <div className="fixed top-20 z-[100] right-5 ">
       <div className="relative inline-flex items-center justify-center">
         {/* SVG for circular progress */}
         <svg className="w-24 h-24 transform -rotate-90 ">
@@ -51,7 +57,7 @@ const CountdownTimer: React.FC<CountdownProps> = ({
             cx="48"
             cy="48"
             r={radius}
-            className="stroke-[#2F004F]"
+            className={strokeStyle}
             strokeWidth="8"
             fill="white"
             strokeLinecap="round"

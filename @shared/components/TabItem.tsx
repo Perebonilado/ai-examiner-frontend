deec1d37bd59d25522db3b348a9e870c7ee5ff2a
perebonilado <eradiribonnie@gmail.com>
2025-05-04 21:27:16 +0100

    current tab fix

--- @shared/components/TabItem.tsx
+++ @shared/components/TabItem.tsx
@@ -13,6 +13,7 @@ const TabItem: FC<Props> = ({ isActive, title, handleClick }) => {
     "font-medium px-16 py-2 max-md:px-8 whitespace-nowrap transition-colors duration-300",
     {
       "text-[#360B58]": isActive,
+      "border-b-[3px] border-b-[#360B58]": isActive,
     }
     // Removed the border styling since we're using the animated indicator
   );

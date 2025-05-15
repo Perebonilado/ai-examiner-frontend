6a544ce9e6c33f2472759321bad438ee62eb22b4
perebonilado <eradiribonnie@gmail.com>
2025-05-16 00:39:53 +0100

    icons and wordings fix

--- @modules/questions/EasyRead/LoadingReader.tsx
+++ @modules/questions/EasyRead/LoadingReader.tsx
@@ -6,7 +6,10 @@ const LoadingReader: FC = () => {
   return (
     <Modal>
       <div className="w-full max-w-[400px] max-sm:max-w-[96vw] flex items-center justify-center p-6 h-[200px] max-sm:h-[230px] shadow-md bg-white border border-dashed rounded-3xl">
-        <FileUploadSpinner title={`One moment... getting your files`} />
+        <FileUploadSpinner
+          title={`One moment... getting your files`}
+          showIcon={false}
+        />
       </div>
     </Modal>
   );

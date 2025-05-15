0fc6b3fb147fe969dda44d0629749c354791c936
perebonilado <eradiribonnie@gmail.com>
2025-05-15 15:30:52 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 const nextConfig = {
   // ... other options you like
   images: {
-    unoptimized: true,
+    unoptimized: false,
     remotePatterns: [
       {
         protocol: "https",

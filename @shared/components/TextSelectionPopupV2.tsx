5fcd9dc86dc2116ee4ccd53fdc69b1e0bf62ce44
perebonilado <eradiribonnie@gmail.com>
2025-07-07 15:54:31 +0100

    bug fix

--- @shared/components/TextSelectionPopupV2.tsx
+++ @shared/components/TextSelectionPopupV2.tsx
@@ -90,11 +90,11 @@ const TextSelectionPopupV2 = forwardRef<
                 onClick={(e) => {
                   const selection = window.getSelection();
                   const selectedText = selection ? selection.toString() : "";
+                  hidePopUp();
                   handleAction({
                     highlight: action.toLowerCase() as HighlightToPromptType,
                     question: selectedText,
                   });
-                  hidePopUp();
                 }}
                 onMouseOver={() => {
                   setIsHovering(true);
